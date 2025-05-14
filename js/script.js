51ccec5f3f04b0bf94381e5b8c66f1d0b2efc88c
geovannaetm <geovanna.t@aluno.ifsp.edu.br>
2025-05-14 20:17:39 -0300

    criando carrosel

--- js/script.js
+++ js/script.js
@@ -75,4 +75,42 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+//CARROSEL
 
+new Swiper('.card-wrapper', {
+  loop: true,
+  spaceBetween: 30,
+
+  // paginação
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      
+    },
+
+     768: {
+      slidesPerView: 2,
+      
+    },
+
+     1024: {
+      slidesPerView: 3,
+      
+    },
+    
+  },
+
+
+});
